afd9c43c7b448ede60fd4d2006a351ff7134c952
Albert <albert.bhagwan@gmail.com>
2020-08-28 14:48:53 +0200

    Change optimizer to Adam

--- ar1star_lat_replay.py
+++ ar1star_lat_replay.py
@@ -109,9 +109,12 @@ if reg_lambda != 0:
 
 # Optimizer setup
 # ABB: change optimizer to Adam
-optimizer = torch.optim.SGD(
-    model.parameters(), lr=init_lr, momentum=momentum, weight_decay=l2
-)
+# optimizer = torch.optim.SGD(
+#     model.parameters(), lr=init_lr, momentum=momentum, weight_decay=l2
+# )
+
+optimizer = torch.optim.Adam(model.parameters(), lr=init_lr, weight_decay=l2)
+
 # ABB: change loss to BCELoss and NLLLoss
 criterion = torch.nn.CrossEntropyLoss()
 
@@ -131,9 +134,11 @@ for i, train_batch in enumerate(dataset):
         change_brn_pars(
             model, momentum=inc_update_rate, r_d_max_inc_step=0,
             r_max=max_r_max, d_max=max_d_max)
-        optimizer = torch.optim.SGD(
-            model.parameters(), lr=inc_lr, momentum=momentum, weight_decay=l2
-        )
+        # optimizer = torch.optim.SGD(
+        #     model.parameters(), lr=inc_lr, momentum=momentum, weight_decay=l2
+        # )
+
+        optimizer = torch.optim.Adam(model.parameters(), lr=inc_lr, weight_decay=l2)
 
     train_x, train_y = train_batch
     train_x = preproc(train_x)
