6fdd57c9efbe7f93a09696ef5af593a41e6f78b4
Albert <albert.bhagwan@gmail.com>
2020-09-02 17:45:17 +0200

    ACGAN MNIST with simple discriminator

--- models/discriminator.py
+++ models/discriminator.py
@@ -31,5 +31,51 @@ class discriminator(nn.Module):
     def forward(self, input):
         source = self.main(input)
         source = source.view(source.shape[0])
-        
+
         return source
+
+class conditioned_discriminator(nn.Module):
+    def __init__(self,nc=3,ndf=64,num_classes=10):
+        super(discriminator, self).__init__()
+
+        self.main = nn.Sequential(
+            # input is (nc) x 128 x 128
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 64 x 64
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 32 x 32
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 16 x 16
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 8 x 8
+            nn.Conv2d(ndf * 8, ndf * 16, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 16),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*16) x 4 x 4
+            nn.Conv2d(ndf * 16, 1, 4, 1, 0, bias=False),
+        )
+        self.fc_dis = nn.Linear(ndf*16*4*4, 1)
+        self.fc_aux = nn.Linear(ndf*16*4*4, num_classes)
+
+        self.softmax = nn.Softmax()
+        self.sig = nn.Sigmoid()
+
+        self.ndf = ndf
+
+    def forward(self, input):
+        features = self.main(input)
+        flat_features = features.view(-1,self.ndf*16*4*4)
+
+        classes = self.softmax(self.fc_aux(flat_features))
+        source = slef.sigmoid(self.fc_dis(flat_features))
+
+        source = source.view(source.shape[0])
+
+        return classes, source
