982c951a4b4429943824dcedfbd167da93f164ff
Albert <albert.bhagwan@gmail.com>
2020-09-03 12:35:30 +0200

    Undone previous commit

--- acgan_mnist_mobilenet.py
+++ acgan_mnist_mobilenet.py
@@ -111,7 +111,7 @@ def weights_init(m):
         nn.init.constant_(m.bias.data, 0)
 
 # Discriminator + classifier
-model = MyMobilenetV1(pretrained=True, latent_layer_num=latent_layer_num, num_classes=n_class, softmax=False)
+model = MyMobilenetV1(pretrained=True, latent_layer_num=latent_layer_num, num_classes=n_class, softmax=True)
 gen = generator(nz)
 
 gen.apply(weights_init)
