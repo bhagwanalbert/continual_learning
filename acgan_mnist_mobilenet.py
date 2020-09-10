58abb9a55d1ea25219f533a8d20a80180b94ac58
Albert <albert.bhagwan@gmail.com>
2020-09-10 10:54:00 +0200

    Soft real/fake labels

--- acgan_mnist_mobilenet.py
+++ acgan_mnist_mobilenet.py
@@ -185,10 +185,10 @@ for ep in range(num_epochs):
 
         # Labels indicating source of the image
         real_label = maybe_cuda(torch.FloatTensor(train_y.size(0)), use_cuda=use_cuda)
-        real_label.fill_(1)
+        real_label.fill_(0.9)
 
         fake_label = maybe_cuda(torch.FloatTensor(train_y.size(0)), use_cuda=use_cuda)
-        fake_label.fill_(0)
+        fake_label.fill_(0.1)
 
         _, pred_label = torch.max(classes, 1)
         correct_cnt += (pred_label == train_y).sum()
