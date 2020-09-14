9fa215cb1e269ab5b94416444544a1443e3ed806
Albert <albert.bhagwan@gmail.com>
2020-09-14 18:17:54 +0200

    Debug

--- acgan_core50_simple.py
+++ acgan_core50_simple.py
@@ -86,7 +86,7 @@ train_x = train_x[indexes]
 train_y = train_y[indexes]
 
 for c in range(n_class):
-    writer.add_image("Training images per class", vutils.make_grid(train_x[train_y.numpy() == c], padding=2, normalize=True).cpu())
+    writer.add_image("Training images per class", vutils.make_grid(train_x[train_y.numpy() == c], padding=2, normalize=False).cpu())
 
 writer.close()
 
