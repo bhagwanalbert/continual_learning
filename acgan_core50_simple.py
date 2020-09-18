757ba2bff756187242c78fc6ccc6a9d8c0927786
Albert <albert.bhagwan@gmail.com>
2020-09-18 16:08:37 +0200

    Added instance norm weights initialization

--- acgan_core50_simple.py
+++ acgan_core50_simple.py
@@ -108,6 +108,9 @@ def weights_init(m):
     elif classname.find('BatchNorm') != -1:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
+    elif classname.find('InstanceNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
 
 # Discriminator + classifier
 model = conditioned_discriminator_v2(num_classes=n_class)
