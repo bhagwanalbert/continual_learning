5588e6aa3c1b3d3a1d95f505117a9b6cb2170ff8
Albert <albert.bhagwan@gmail.com>
2020-09-22 15:55:00 +0200

    Changed generator (bilinear interp + conv)

--- models/generator.py
+++ models/generator.py
@@ -20,27 +20,33 @@ class generator(nn.Module):
 
         self.main = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d( nz, ngf * 16, 4, 1, 0, bias=False),
+            Interpolate(size=(4, 4), mode='bilinear'),
+            nn.Conv2d(nz, ngf*16, 3, 1, 1, bias=False),
             nn.BatchNorm2d(ngf * 16),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ngf*16) x 4 x 4
-            nn.ConvTranspose2d(ngf * 16, ngf * 8, 4, 2, 1, bias=False),
+            Interpolate(size=(8, 8), mode='bilinear'),
+            nn.Conv2d(ngf*16, ngf*8, 3, 1, 1, bias=False),
             nn.BatchNorm2d(ngf * 8),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ngf*8) x 8 x 8
-            nn.ConvTranspose2d( ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            Interpolate(size=(16, 16), mode='bilinear'),
+            nn.Conv2d(ngf*8, ngf*4, 3, 1, 1, bias=False),
             nn.BatchNorm2d(ngf * 4),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ngf*4) x 16 x 16
-            nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            Interpolate(size=(32, 32), mode='bilinear'),
+            nn.Conv2d(ngf*4, ngf*2, 3, 1, 1, bias=False),
             nn.BatchNorm2d(ngf * 2),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ngf*2) x 32 x 32
-            nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
+            Interpolate(size=(64, 64), mode='bilinear'),
+            nn.Conv2d(ngf*2, ngf, 3, 1, 1, bias=False),
             nn.BatchNorm2d(ngf),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ngf) x 64 x 64
-            nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
+            Interpolate(size=(128, 128), mode='bilinear'),
+            nn.Conv2d(ngf, nc, 3, 1, 1, bias=False),
             nn.Tanh()
             # state size. (nc) x 128 x 128
 
