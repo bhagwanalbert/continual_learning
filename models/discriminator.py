b12c047a6ac4f2eb84d6fb05dfe7fcea989a42a5
Albert <albert.bhagwan@gmail.com>
2020-09-22 12:50:11 +0200

    Added instance norm to discriminator

--- models/discriminator.py
+++ models/discriminator.py
@@ -44,19 +44,19 @@ class conditioned_discriminator(nn.Module):
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf) x 64 x 64
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 2),
+            nn.InstanceNorm2d(ndf * 2, affine = True, track_running_stats=True),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*2) x 32 x 32
             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 4),
+            nn.InstanceNorm2d(ndf * 4, affine = True, track_running_stats=True),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*4) x 16 x 16
             nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 8),
+            nn.InstanceNorm2d(ndf * 8, affine = True, track_running_stats=True),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*8) x 8 x 8
             nn.Conv2d(ndf * 8, ndf * 16, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 16),
+            nn.InstanceNorm2d(ndf * 16, affine = True, track_running_stats=True),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*16) x 4 x 4
             # nn.Conv2d(ndf * 16, 1, 4, 1, 0, bias=False),
