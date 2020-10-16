6fc13e7bca0af67c26983210637753fba7c40f93
Albert <albert.bhagwan@gmail.com>
2020-10-16 11:30:17 +0200

    Quick memory check

--- biggan_core50_simple.py
+++ biggan_core50_simple.py
@@ -35,7 +35,7 @@ dataset = CORE50(root='/home/abhagwan/datasets/core50', scenario="nicv2_391")
 workers = 2
 
 # Batch size during training
-batch_size = 100
+batch_size = 50
 
 # Spatial size of training images. All images will be resized to this
 #   size using a transformer.
