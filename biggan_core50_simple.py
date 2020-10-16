ad1ed25fc0100b8e22044ace6d45ab62c5be75f0
Albert <albert.bhagwan@gmail.com>
2020-10-16 12:30:52 +0200

    Debug

--- biggan_core50_simple.py
+++ biggan_core50_simple.py
@@ -121,8 +121,8 @@ ema = ema(G, G_ema,start_itr = 20000)
 G.optim.load_state_dict(
       torch.load('%s/%s.pth' % (weight_root, 'G_optim')))
 
-for name, param in G.optim.named_parameters():
-    print(name)
+for var_name in G.optim.state_dict():
+    print(var_name, "\t", G.optim.state_dict()[var_name])
 
 print(G.optim.state_dict())
 
