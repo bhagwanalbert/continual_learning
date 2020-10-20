6208f65ce79772e50ec971f5904c38fac2485a32
Albert <albert.bhagwan@gmail.com>
2020-10-20 09:30:34 +0200

    Send images to tensorboard only once per epoch

--- biggan_core50_simple.py
+++ biggan_core50_simple.py
@@ -312,7 +312,7 @@ for ep in range(num_epochs):
         writer.add_scalar('D_loss_fake', float(D_loss_fake.item()), tot_it_step)
         writer.close()
 
-        with torch.no_grad():
-            fake = nn.parallel.data_parallel(G, (eval_z, G.shared(eval_y)), device_ids=[2, 3, 0, 1])
-        writer.add_image("Generated images", vutils.make_grid(fake, nrow=n_imag, padding=2, normalize=True))
-        writer.close()
+    with torch.no_grad():
+        fake = nn.parallel.data_parallel(G, (eval_z, G.shared(eval_y)), device_ids=[2, 3, 0, 1])
+    writer.add_image("Generated images", vutils.make_grid(fake, nrow=n_imag, padding=2, normalize=True))
+    writer.close()
