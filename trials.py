661e1ccab1ce2903b4fa8d8e9fdd5e3c9d3599b6
Albert <albert.bhagwan@gmail.com>
2020-10-27 13:27:11 +0100

    Print BigGAN model

--- trials.py
+++ trials.py
@@ -1,20 +1,22 @@
-from models import generator
+from models import BigGAN
 import numpy as np
 import torch
 from torch import nn
 
-nz = 110
-batch_size = 100
+n_class = 50
 
-gen = generator.generator(nz)
 
-eval_noise = torch.FloatTensor(batch_size, nz, 1, 1).normal_(0, 1)
-eval_noise_ = np.random.normal(0, 1, (batch_size, nz))
-eval_label = np.random.randint(0, 10, batch_size)
-eval_onehot = np.zeros((batch_size, 10))
-eval_onehot[np.arange(batch_size), eval_label] = 1
-eval_noise_[np.arange(batch_size), :10] = eval_onehot[np.arange(batch_size)]
-eval_noise_ = (torch.from_numpy(eval_noise_))
-eval_noise.data.copy_(eval_noise_.view(batch_size, nz, 1, 1))
+G = BigGAN.Generator(n_classes = n_class)
+D = BigGAN.Discriminator(n_classes = n_class)
 
-print(gen(eval_noise).shape)
+print(G)
+
+for name, param in G.named_parameters():
+    # tells whether we want to use gradients for a given parameter
+    print(name)
+
+print(D)
+
+for name, param in D.named_parameters():
+    # tells whether we want to use gradients for a given parameter
+    print(name)
