73ae84760b79acf9acad9aa824048174c1caa415
Albert <albert.bhagwan@gmail.com>
2020-11-04 10:14:30 +0100

    Change trials.py

--- trials.py
+++ trials.py
@@ -4,6 +4,8 @@ import torch
 from torch import nn
 
 n_class = 50
+# Root of weights
+weight_root = './weights'
 
 
 G = BigGAN.Generator(n_classes = n_class)
@@ -12,11 +14,21 @@ D = BigGAN.Discriminator(n_classes = n_class)
 print(G)
 
 for name, param in G.named_parameters():
-    # tells whether we want to use gradients for a given parameter
     print(name)
 
 print(D)
 
 for name, param in D.named_parameters():
-    # tells whether we want to use gradients for a given parameter
     print(name)
+
+G.optim.load_state_dict(
+      torch.load('%s/%s.pth' % (weight_root, 'G_optim')))
+
+for param_group in G.optim.state_dict():
+    print(param_group)
+
+D.optim.load_state_dict(
+      torch.load('%s/%s.pth' % (weight_root, 'D_optim')))
+
+for param_group in D.optim.state_dict():
+    print(param_group)
