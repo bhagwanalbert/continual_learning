f19c9cac8962c9442ac5f1372f57db30726865c8
Albert <albert.bhagwan@gmail.com>
2020-11-05 11:55:56 +0100

    More trials

--- trials.py
+++ trials.py
@@ -16,27 +16,31 @@ print(G)
 for name, param in G.named_parameters():
     print(name)
 
+print(G.parameters())
+
 print(D)
 
 for name, param in D.named_parameters():
     print(name)
 
-G.optim.load_state_dict(
-      torch.load('%s/%s.pth' % (weight_root, 'G_optim')))
+print(D.parameters())
 
-for param_group in G.optim.state_dict():
-    print(param_group)
-
-print(G.optim.state_dict()['param_groups'])
-
-D.optim.load_state_dict(
-      torch.load('%s/%s.pth' % (weight_root, 'D_optim')))
-
-for param_group in D.optim.state_dict():
-    print(param_group)
-
-print(D.optim.state_dict()['param_groups'])
-
-for param_group in D.optim.state_dict()['state']:
-    for param in D.optim.state_dict()['state'][param_group]:
-        print(D.optim.state_dict()['state'][param_group][param])
+# G.optim.load_state_dict(
+#       torch.load('%s/%s.pth' % (weight_root, 'G_optim')))
+#
+# for param_group in G.optim.state_dict():
+#     print(param_group)
+#
+# print(G.optim.state_dict()['param_groups'])
+#
+# D.optim.load_state_dict(
+#       torch.load('%s/%s.pth' % (weight_root, 'D_optim')))
+#
+# for param_group in D.optim.state_dict():
+#     print(param_group)
+#
+# print(D.optim.state_dict()['param_groups'])
+#
+# for param_group in D.optim.state_dict()['state']:
+#     for param in D.optim.state_dict()['state'][param_group]:
+#         print(D.optim.state_dict()['state'][param_group][param])
