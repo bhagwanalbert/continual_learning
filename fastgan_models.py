59dbf5e438fb0670f11663b8b741f9af7190ba02
albert_bhagwan <albert.bhagwan@gmail.com>
2020-11-29 17:06:00 +0100

    Forward with only one input (bug)

--- fastgan_models.py
+++ fastgan_models.py
@@ -38,7 +38,7 @@ class Reshape(nn.Module):
 
     def forward(self, feat):
         batch = feat.shape[0]
-        return feat.view(batch, *self.target_shape)        
+        return feat.view(batch, *self.target_shape)
 
 class GLU(nn.Module):
     def forward(self, x):
@@ -68,7 +68,7 @@ class SEBlock(nn.Module):
     def __init__(self, ch_in, ch_out):
         super().__init__()
 
-        self.main = nn.Sequential(  nn.AdaptiveAvgPool2d(4), 
+        self.main = nn.Sequential(  nn.AdaptiveAvgPool2d(4),
                                     conv2d(ch_in, ch_out, 4, 1, 0, bias=False), Swish(),
                                     conv2d(ch_out, ch_out, 1, 1, 0, bias=False), nn.Sigmoid() )
 
@@ -130,28 +130,28 @@ class Generator(nn.Module):
         self.im_size = im_size
 
         self.init = InitLayer(nz, channel=ngf*16)
-                                
+
         self.feat_8 = UpBlockComp(nfc[4], nfc[8])
         self.feat_16 = UpBlock(nfc[8], nfc[16])
         self.feat_32 = UpBlockComp(nfc[16], nfc[32])
         self.feat_64 = UpBlock(nfc[32], nfc[64])
-        self.feat_128 = UpBlockComp(nfc[64], nfc[128])  
-        self.feat_256 = UpBlock(nfc[128], nfc[256]) 
+        self.feat_128 = UpBlockComp(nfc[64], nfc[128])
+        self.feat_256 = UpBlock(nfc[128], nfc[256])
 
         self.se_128 = SEBlock(nfc[8], nfc[128])
         self.se_256 = SEBlock(nfc[16], nfc[256])
 
         self.to_128 = nn.Sequential( conv2d(nfc[128], nc, 1, 1, 0, bias=False) , nn.Tanh() )
         self.to_big = nn.Sequential( conv2d(nfc[im_size], nc, 3, 1, 1, bias=False) , nn.Tanh() )
-        
+
         if im_size > 256:
-            self.feat_512 = UpBlockComp(nfc[256], nfc[512]) 
+            self.feat_512 = UpBlockComp(nfc[256], nfc[512])
             self.se_512 = SEBlock(nfc[32], nfc[512])
         if im_size > 512:
-            self.feat_1024 = UpBlock(nfc[512], nfc[1024])  
-        
+            self.feat_1024 = UpBlock(nfc[512], nfc[1024])
+
     def forward(self, input):
-        
+
         feat_4 = self.init(input)
         feat_8 = self.feat_8(feat_4)
         feat_16 = self.feat_16(feat_8)
@@ -161,7 +161,7 @@ class Generator(nn.Module):
         feat_256 = self.se_256( feat_16, self.feat_256(feat_128) )
         if self.im_size == 256:
             return [self.to_big(feat_256), self.to_128(feat_128)]
-        
+
         feat_512 = self.se_512( feat_32, self.feat_512(feat_256) )
         if self.im_size == 512:
             return [self.to_big(feat_512), self.to_128(feat_128)]
@@ -196,18 +196,18 @@ class Discriminator(nn.Module):
         self.im_size = im_size
 
         if im_size == 1024:
-            self.down_from_big = nn.Sequential( 
+            self.down_from_big = nn.Sequential(
                                     conv2d(nc, ndf//8, 4, 2, 1, bias=False),
                                     nn.LeakyReLU(0.2, inplace=True),
                                     conv2d(ndf//8, ndf//4, 4, 2, 1, bias=False),
                                     batchNorm2d(ndf//4),
                                     nn.LeakyReLU(0.2, inplace=True))
         elif im_size == 512:
-            self.down_from_big = nn.Sequential( 
+            self.down_from_big = nn.Sequential(
                                     conv2d(nc, ndf//4, 4, 2, 1, bias=False),
                                     nn.LeakyReLU(0.2, inplace=True) )
         elif im_size == 256:
-            self.down_from_big = nn.Sequential( 
+            self.down_from_big = nn.Sequential(
                                     conv2d(nc, ndf//4, 3, 1, 1, bias=False),
                                     nn.LeakyReLU(0.2, inplace=True) )
 
@@ -226,7 +226,7 @@ class Discriminator(nn.Module):
         self.se_2_16 = SEBlock(ndf//4, ndf*2)
         self.se_4_32 = SEBlock(ndf//2, ndf*4)
         self.se_8_64 = SEBlock(ndf*1, ndf*16)
-        
+
         self.down_from_small = nn.Sequential( conv2d(nc, ndf//2, 4, 2, 1, bias=False), nn.LeakyReLU(0.2, inplace=True),
                                             DownBlock(ndf//2,  ndf*1),
                                             DownBlock(ndf*1,  ndf*2),
@@ -236,20 +236,23 @@ class Discriminator(nn.Module):
         self.decoder_big = SimpleDecoder(ndf*16, nc)
         self.decoder_part = SimpleDecoder(ndf*4, nc)
         self.decoder_small = SimpleDecoder(ndf*4, nc)
-        
-    def forward(self, imgs, label):
+
+    def forward(self, input_dict):
+        imgs = input_dict["data"]
+        label = input_dict["label"]
+
         if type(imgs) is not list:
             imgs = [F.interpolate(imgs, size=self.im_size), F.interpolate(imgs, size=128)]
-        feat_2 = self.down_from_big(imgs[0])        
+        feat_2 = self.down_from_big(imgs[0])
         feat_4 = self.down_4(feat_2)
         feat_8 = self.down_8(feat_4)
-        
+
         feat_16 = self.down_16(feat_8)
         feat_16 = self.se_2_16(feat_2, feat_16)
 
         feat_32 = self.down_32(feat_16)
         feat_32 = self.se_4_32(feat_4, feat_32)
-        
+
         feat_last = self.down_64(feat_32)
         feat_last = self.se_8_64(feat_8, feat_last)
 
@@ -258,7 +261,7 @@ class Discriminator(nn.Module):
         feat_small = self.down_from_small(imgs[1])
         rf_1 = self.rf_from_128(feat_small)
 
-        if label=='real':    
+        if label=='real':
             rec_img_big = self.decoder_big(feat_last)
             rec_img_small = self.decoder_small(feat_small)
 
@@ -273,9 +276,9 @@ class Discriminator(nn.Module):
             if part==3:
                 rec_img_part = self.decoder_part(feat_32[:,:,8:,8:])
 
-            return torch.cat([rf_0, rf_1], dim=1) , [rec_img_big, rec_img_small, rec_img_part], part 
+            return torch.cat([rf_0, rf_1], dim=1) , [rec_img_big, rec_img_small, rec_img_part], part
 
-        return torch.cat([rf_0, rf_1], dim=1) 
+        return torch.cat([rf_0, rf_1], dim=1)
 
 
 
@@ -288,7 +291,7 @@ class SimpleDecoder(nn.Module):
         nfc = {}
         for k, v in nfc_multi.items():
             nfc[k] = int(v*32)
-                 
+
         self.main = nn.Sequential(  nn.AdaptiveAvgPool2d(8),
                                     UpBlock(nfc_in, nfc[16]) ,
                                     UpBlock(nfc[16], nfc[32]),
