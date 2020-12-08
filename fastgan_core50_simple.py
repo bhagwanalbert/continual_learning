475a480c4c7d3d79a2d4aa7d321203e1076dd97c
Albert Bhagwan <albert.bhagwan@gmail.com>
2020-12-08 14:27:57 +0100

    Previous bug solved, removing debugging lines

--- fastgan_core50_simple.py
+++ fastgan_core50_simple.py
@@ -224,8 +224,6 @@ def train(args):
             netD.zero_grad()
 
             err_dr_real, rec_img_all, rec_img_small, rec_img_part, err_class_real = train_d(netD, real_image, y_mb, label="real")
-            print(correct_cnt)
-            print("---------")
             class_acc = correct_cnt.item() / data_encountered
             err_dr_fake, err_class_fake = train_d(netD, [fi.detach() for fi in fake_images], label, label="fake")
             optimizerD.step()
