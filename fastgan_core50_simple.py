c22fdf991fad074b169794411dc3fa9019ae39ce
Albert Bhagwan <albert.bhagwan@gmail.com>
2020-12-08 12:08:50 +0100

    Minor change

--- fastgan_core50_simple.py
+++ fastgan_core50_simple.py
@@ -236,7 +236,7 @@ def train(args):
                 backup_para = copy_G_params(netG)
                 load_params(netG, avg_param_G)
                 with torch.no_grad():
-                    vutils.save_image(netG(fixed_noise)[0].add(1).mul(0.5), saved_image_folder+'/%d.jpg'%tot_it_step, nrow=5)
+                    vutils.save_image(netG(fixed_noise)[0].add(1).mul(0.5), saved_image_folder+'/%d.jpg'%tot_it_step, nrow=n_imag)
                     vutils.save_image( torch.cat([
                             F.interpolate(real_image, 128),
                             rec_img_all, rec_img_small,
