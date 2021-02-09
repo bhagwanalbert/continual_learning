11d0e8ad91202b492afccb03be27bbfb4e0e6857
Albert Bhagwan <albert.bhagwan@gmail.com>
2021-02-09 20:27:44 +0100

    Debug

--- fastgan_core50_full.py
+++ fastgan_core50_full.py
@@ -371,6 +371,18 @@ def train(args):
             del prev_x
             torch.cuda.empty_cache()
 
+        # Generate images for debugging
+        backup_para = copy_G_params(netG)
+        load_params(netG, avg_param_G)
+        with torch.no_grad():
+            writer.add_image("Generated images C0-9", vutils.make_grid(netG(fixed_noise[0:n_imag*10])[0].add(1).mul(0.5), nrow=n_imag, padding=2, normalize=True))
+            writer.add_image("Generated images C10-19", vutils.make_grid(netG(fixed_noise[n_imag*10:n_imag*20])[0].add(1).mul(0.5), nrow=n_imag, padding=2, normalize=True))
+            writer.add_image("Generated images C20-29", vutils.make_grid(netG(fixed_noise[n_imag*20:n_imag*30])[0].add(1).mul(0.5), nrow=n_imag, padding=2, normalize=True))
+            writer.add_image("Generated images C30-39", vutils.make_grid(netG(fixed_noise[n_imag*30:n_imag*40])[0].add(1).mul(0.5), nrow=n_imag, padding=2, normalize=True))
+            writer.add_image("Generated images C40-49", vutils.make_grid(netG(fixed_noise[n_imag*40:n_imag*50])[0].add(1).mul(0.5), nrow=n_imag, padding=2, normalize=True))
+            writer.close()
+        load_params(netG, backup_para)
+
         for ep in range(num_epochs):
             print("training ep: ", ep)
             data_encountered = 0
