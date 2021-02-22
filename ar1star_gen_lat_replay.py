3f04b52533ca84009ae3a14e1f8283ce82a89834
Albert Bhagwan <albert.bhagwan@gmail.com>
2021-02-22 17:56:31 +0100

    Different noise for G and D

--- ar1star_gen_lat_replay.py
+++ ar1star_gen_lat_replay.py
@@ -403,6 +403,21 @@ for i, train_batch in enumerate(dataset):
             lossDfake.backward()
             optimD.step()
 
+            noise = torch.FloatTensor(y_mb.size(0), nz, 1, 1).normal_(0, 1)
+            noise_ = np.random.normal(0, 1, (y_mb.size(0), nz))
+            label = np.random.choice(cur_class, y_mb.size(0))
+            onehot = np.zeros((y_mb.size(0), n_class))
+            onehot[np.arange(y_mb.size(0)), label] = 1
+            noise_[np.arange(y_mb.size(0)), :n_class] = onehot[np.arange(y_mb.size(0))]
+            noise_ = (torch.from_numpy(noise_))
+            noise.data.copy_(noise_.view(y_mb.size(0), nz, 1, 1))
+            noise = maybe_cuda(noise, use_cuda=use_cuda)
+
+            label = ((torch.from_numpy(label)).long())
+            label = maybe_cuda(label, use_cuda=use_cuda)
+
+            fake_feat = gen(noise)
+
             optimG.zero_grad()
 
             classes, source = disc(fake_feat.detach())
